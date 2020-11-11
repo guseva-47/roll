220732188dabfc7ead61299b368ff2cf794bc064
lenivoe <levishok@gmail.com>
2020-11-12 01:41:22 +0700

    tests of google oauth2 availability

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,18 +15,42 @@ describe('AppController (e2e)', () => {
         await app.init();
     });
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('hello');
+    describe('simplest test for test', () => {
+        it('/ (GET)', async () => {
+            await request(app.getHttpServer())
+                .get('/')
+                .expect(200)
+                .expect('hello');
+        });
     });
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('hello');
+    describe('redirecting to google for login', () => {
+        beforeEach(() => console.log('>>> \u03A9 <<<'))
+
+        it('/login (GET)', async () => {
+            await request(app.getHttpServer())
+                .get('/login')
+                .expect(302)
+                .expect('location', '/auth/google')
+        });
+
+        it('/auth/google (GET)', async () => {
+            await request(app.getHttpServer())
+                .get('/auth/google')
+                .expect(302)
+                .expect('location', /https:\/\/accounts\.google\.com\/o\/oauth2\/v2\/auth*/)
+        });
+
+        it('/login (GET) with redirect', async () => {
+            const redirectToRes = await request(app.getHttpServer())
+                .get('/login')
+                .expect(302)
+                .redirects(1)
+                .expect(302)
+        });
     });
 
+    afterEach(async () => {
+        await app.close();
+    });
 });
